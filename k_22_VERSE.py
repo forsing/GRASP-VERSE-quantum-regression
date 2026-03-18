11d2686460cf8bd1a94670963c58b1546446a8dc
ForsIng <forsing@gmail.com>
2026-03-18 17:17:09 +0100

    Update k_22_VERSE.py

--- k_22_VERSE.py
+++ k_22_VERSE.py
@@ -23,7 +23,7 @@ from qiskit_machine_learning.utils import algorithm_globals
 from qiskit.circuit.library import ZZFeatureMap, TwoLocal
 from qiskit.quantum_info import Statevector, Pauli
 
-CSV_PATH = "/Users/4c/Desktop/GHQ/data/loto7hh_4580_k21.csv"
+CSV_PATH = "/data/loto7hh_4580_k21.csv"
 
 df = pd.read_csv(CSV_PATH)
 print()
@@ -207,7 +207,7 @@ def main():
     print()
     """
     Predikcija (VERSE + kvantna regresija, deterministički, strukturno):
-    (7, 8, 11, 23, 26, 33, 34)
+    (7, 8, x, y, z, 33, 34)
 
     Score: 0.5033166996071922
     """
